9ac880fd0af451e395b5c5d82731cbc61bbf5eae
sachinviraj <74185781+Sviraj@users.noreply.github.com>
2024-03-13 12:00:09 +0530

    commit intro changes

--- components/intro.tsx
+++ components/intro.tsx
@@ -33,7 +33,7 @@ export default function Intro() {
           >
             <Image
               src={myImage}
-              alt="Ricardo portrait"
+              alt="Sachin portrait"
               width="192"
               height="192"
               quality="95"
@@ -104,6 +104,14 @@ export default function Intro() {
           target="_blank"
         >
           <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/Sviraj"
+          target="_blank"
+        >
+          <FaGithubSquare />
         </a>
 
         <a
